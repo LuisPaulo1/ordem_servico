af96456452a410e94d7c62366f22910fd0a22cef
LuisPaulo1 <csluispaulo@gmail.com>
2024-02-16 17:12:33 -0300

    Exceção para CPF já cadastrado

--- backend/src/main/java/com/udemy/ordem_servico/repositories/TecnicoRepository.java
+++ backend/src/main/java/com/udemy/ordem_servico/repositories/TecnicoRepository.java
@@ -2,8 +2,12 @@ package com.udemy.ordem_servico.repositories;
 
 import com.udemy.ordem_servico.domain.Tecnico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TecnicoRepository extends JpaRepository<Tecnico, Integer> {
+    @Query("SELECT obj FROM Tecnico obj WHERE obj.cpf =:cpf")
+    Tecnico findByCpf(@Param("cpf") String cpf);
 }
--- backend/src/main/java/com/udemy/ordem_servico/service/TecnicoService.java
+++ backend/src/main/java/com/udemy/ordem_servico/service/TecnicoService.java
@@ -3,11 +3,13 @@ package com.udemy.ordem_servico.service;
 import com.udemy.ordem_servico.domain.Tecnico;
 import com.udemy.ordem_servico.domain.dtos.TecnicoDTO;
 import com.udemy.ordem_servico.repositories.TecnicoRepository;
+import com.udemy.ordem_servico.service.exceptions.DataIntegratyViolationException;
 import com.udemy.ordem_servico.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -27,8 +29,19 @@ public class TecnicoService {
     }
 
     public TecnicoDTO create(TecnicoDTO obj) {
+        if (Objects.requireNonNull(findByCpf(obj)).getClass().equals(Tecnico.class))
+            throw new DataIntegratyViolationException("CPF já cadastrado na base de dados!");
+
         Tecnico tecnico = new Tecnico(null, obj.getNome(), obj.getCpf(), obj.getTelefone());
         return new TecnicoDTO(repository.save(tecnico));
+    }
+
+    private Tecnico findByCpf(TecnicoDTO objDTO) {
+        Tecnico obj = repository.findByCpf(objDTO.getCpf());
+        if (obj != null) {
+            return obj;
+        }
+        return null;
     }
 
 }
